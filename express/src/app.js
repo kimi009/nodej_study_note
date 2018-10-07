ddf67ea2d84b88c7e82691065135c2c0f1ab73f8
chenfuye <444812002@qq.com>
2018-10-07 23:11:46 +0800

    commit

--- express/src/app.js
+++ express/src/app.js
@@ -1,28 +1,33 @@
 const express = require('express')
 
-const bodyParse = require('body-parser')
+const approuter = require('./approute.js');
+
+// const bodyParse = require('body-parser')
 
 const app = new express();
 
-app.use(bodyParse.urlencoded({
-  extended: false
-}));
+// app.use(bodyParse.urlencoded({
+//   extended: false
+// }));
 
-app.use(bodyParse.json())
+// app.use(bodyParse.json())
 
-app.set('view engine', 'ejs')
+// app.set('view engine', 'ejs')
 
-app.get('/', function (req, res) {
-  res.send('ok')
-})
+app.use('/admin',approuter);
+// app.get('/', function (req, res) {
+//   res.send('ok')
+// })
 
-app.get('/login', function (req, res) {
-  res.render('login')
-})
+// app.get('/login', function (req, res) {
+//   res.render('login')
+// })
 
-app.post('/doLogin', function (req, res) {
-  console.log(req.body);
-})
+// app.post('/doLogin', function (req, res) {
+//   console.log(req.body);
+// })
+
+
 
 
 app.listen(3008, '127.0.0.1')
---
+++ koa/DbHelper/config.js
@@ -0,0 +1,7 @@
+const config = {
+  dbUrl: 'mongodb://127.0.0.1:27017',
+
+  dbName: 'koa'
+}
+
+module.exports = config;

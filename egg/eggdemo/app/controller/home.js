b20587f04b1e8a241fab3d4d0fbb2ca19cbfdb9d
chenfuye <444812002@qq.com>
2018-10-11 19:03:37 +0800

    commit

--- egg/eggdemo/app/controller/home.js
+++ egg/eggdemo/app/controller/home.js
@@ -5,9 +5,7 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     let data = await this.service.home.getData();
-    await this.ctx.render('index', {
-      data
-    });
+    await this.ctx.render('home/index', data);
   }
 }
 

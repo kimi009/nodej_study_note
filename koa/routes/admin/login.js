070b7916cf16fa87bca4679c7889e8c581ba08cb
chenfuye <444812002@qq.com>
2018-11-18 20:00:50 +0800

    commit

--- koa/routes/admin/login.js
+++ koa/routes/admin/login.js
@@ -1,27 +1,49 @@
 const router = require('koa-router')(),
   tools = require('../../model/tools'),
-  db = require('../../model/DbHelper/db')
+  db = require('../../model/DbHelper/db'),
+  svgCaptcha = require('svg-captcha');
 
 router.get('/', async(ctx) => {
   await ctx.render('admin/login');
+})
+
+router.get('/code', async(ctx) => {
+  let captcha = svgCaptcha.createMathExpr({
+    size: 6,
+    noise: 3,
+    width: 120,
+    height: 33,
+    color: true,
+    background: '#cc9966'
+  });
+  ctx.session.code = captcha.text;
+  ctx.response.type = 'image/svg+xml'
+  ctx.body = captcha.data;
 })
 
 router.post('/dologin', async(ctx) => {
   // console.log(tools.md5('123456'));
   let {
     username,
-    password
+    password,
+    code
   } = ctx.request.body;
-
-  let res = await db.find('admin', {
-    "username": username,
-    "password": tools.md5(password)
-  });
-  if(res.length){
-    ctx.session.userinfo = res[0];
-    await ctx.redirect('/admin')
-  }else{
-    await ctx.render('admin/login')
+  if (code === ctx.session.code) {
+    let res = await db.find('admin', {
+      "username": username,
+      "password": tools.md5(password)
+    });
+    if (res.length) {
+      ctx.session.userinfo = res[0];
+      await ctx.redirect('/admin')
+    } else {
+      await ctx.render('admin/login')
+    }
+  } else {
+    console.log('验证码失败')
+    await ctx.render('admin/login', {
+      codeError: true
+    });
   }
 })
 
